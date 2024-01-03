51f5a4bbc951367d1b0c3962cfa5f2aeb38ffe46
Jared Guevara <jaredpaulgue@gmail.com>
2024-01-02 16:49:54 -0800

    implement end of game by finishin text prompt

--- typing.js
+++ typing.js
@@ -1,7 +1,7 @@
 const quotes = [
     "You're right, Mahito. I'm you. I wanted to reject you, convince myself that you were wrong. But that doesn't matter now. I'm gonna kill you. Even if you come back as another curse, I'll kill you. Change your name. Change your form. I'll kill you again. I don't need to find meaning or a reason. Maybe in a hundred years after my death, the meaning behind my actions will become apparent. In the grand scheme of things, I'm probably nothing more than a cog. But I'll keep killing curses for as long as I can. That's my role in all this.",
-    "I'm just a cog, and cogs have a function. My function is getting rid of curses and if you're necessary for that, I won't give up until you agree. But, senpai what's your function?",
-    "The sound of the gion shoja bells echoes the impermanence of all things. The colors of sala flowers reveals the truth that the prosperous must decline. However, we are the exception!",
+    "I'm just a cog, and cogs have a function. My function is getting rid of curses and if you're necessary for that, I won't give up until you agree.",
+    "The sound of the gion shoja bells echoes the impermanence of all things. The colors of sala flowers reveals the truth that the prosperous must decline. However, we are the exception.",
     "You're a strong kid, so help others. Even if it's only those closest to you, just save the people you can. It's okay if you lose your way, and don't worry about whether they'll thank you or not. Just save as many people as you can, even if it's only one. When you die, you should be surrounded by people. Don't end up like me."
 ];
 const numQuotes = quotes.length;
@@ -75,7 +75,7 @@ document.getElementById('game').addEventListener('keydown', ev => {
     const isBackspace = key === 'Backspace';
     const isFirstLetter = currentLetter === currentWord.firstChild;
     const isExtra = document.querySelector('.incorrect.extra');
-    const lastCorrectLetter = document.querySelector('.letter .correct:last-of-type');
+    const gameEnd = currentWord.nextElementSibling === null && currentLetter.nextSibling === null
 
     if (document.querySelector('#game.over')) {
         return;
@@ -100,9 +100,16 @@ document.getElementById('game').addEventListener('keydown', ev => {
         }, 100);
     }
 
+    if (gameEnd) {
+        // End game if last word is completed
+        gameOver();
+        return;
+    }
+
     // If input key is a letter, check whether it is correct/incorrect
     if (isLetter) {
         if (currentLetter) {
+            // Validate or invalidate current key and move to next letter
             addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
             removeClass(currentLetter, 'current');
             if (currentLetter.nextSibling) {
@@ -110,6 +117,7 @@ document.getElementById('game').addEventListener('keydown', ev => {
             }
         }
         else {
+            // If no current letter (meaning space is required), mark subsequent keys as 'extra'
             const incorrectLetter = document.createElement('span');
             incorrectLetter.innerHTML = key;
             incorrectLetter.className = 'letter incorrect extra';
@@ -120,6 +128,7 @@ document.getElementById('game').addEventListener('keydown', ev => {
     // If input key is a space, check whether it is correct or incorrect
     if (isSpace) {
         if (expected !== ' ') {
+            // If space is pressed when it is not expected, invalidate all letters of current word and skip to start of next word
             const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
             lettersToInvalidate.forEach(letter => {
                 addClass(letter, 'incorrect');
@@ -137,17 +146,14 @@ document.getElementById('game').addEventListener('keydown', ev => {
     if (isBackspace) {
         if (currentLetter && isFirstLetter) {
             if (currentWord.previousSibling.children.length === currentWord.previousSibling.querySelectorAll('.correct').length) {
+                // If backspace is pressed to traverse into an already correct word, do nothing
                 ;
             }
             else {
-                // Make previous word's last letter the current letter
+                // Make previous word's last letter the current letter if last word has an incorrect letter
                 removeClass(currentWord, 'current');
                 addClass(currentWord.previousSibling, 'current');
                 removeClass(currentLetter, 'current');
-                addClass(lastCorrectLetter, 'current');
-                document.getElementById('cursor').style.left = currentWord.getBoundingClientRect().right - 1 +'px'
-                removeClass(lastCorrectLetter, 'incorrect');
-                removeClass(lastCorrectLetter, 'correct');
             }
         }
         if (currentLetter && !isFirstLetter) {
@@ -172,16 +178,14 @@ document.getElementById('game').addEventListener('keydown', ev => {
         }
     }
 
-    /*if (currentWord.lastChild === currentLetter && !currentLetter.parentElement.nextSibling) {
-        gameOver();
-    }*/
-
     // Moving cursor
     const nextLetter = document.querySelector('.letter.current');
     const nextWord = document.querySelector('.word.current');
     const cursor = document.getElementById('cursor');
     cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 5 + 'px';
+        // Bound cursor top to top of next letter/word
     cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] - 1 + 'px';
+        // Bound cursor left to right of next letter, left of next word
 
 
     // Moving and scrolling lines of text
@@ -192,6 +196,7 @@ document.getElementById('game').addEventListener('keydown', ev => {
     }
 })
 
+// New game button functionality
 document.getElementById('new-game').addEventListener('click', () => {
     gameOver();
     newGame();
